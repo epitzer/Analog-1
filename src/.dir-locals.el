18fac9fbd5b826fcaa6818b6e9609c43f3e085c5
Erik Pitzer <erik.pitzer@gmail.com>
2016-07-25 13:00:40 +0200

    add some configuration

---
+++ src/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  (flycheck-gcc-include-path . ("/home/erik/.pebble-sdk/SDKs/3.11.1/sdk-core/pebble/basalt/include" ".."))
+  (flycheck-clang-include-path . ("/home/erik/.pebble-sdk/SDKs/3.11.1/sdk-core/pebble/basalt/include" ".."))))
